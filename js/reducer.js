8818ed7401cecddb7249314b011095a7b9edefef
allisoncortez <allizetorc@gmail.com>
2020-07-22 19:05:19 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,5 @@
+let state = {count: 0}
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +9,22 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+function render(){
+  document.body.textContent = state.count
+}
+
+//dispatch:persists changes to our state
+//function breakdown:
+//1. called our reducer
+//2. then we took the return value from the reducer AND
+//3. assigned it to be our new state.
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+ 
+render()
+
+dispatch({type: 'INCREASE_COUNT'})
